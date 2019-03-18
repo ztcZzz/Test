bd7293e02a2b0bed7514ea34b7769592d643714c
ztcZzz <18616320504@163.com>
2019-03-18 23:39:35 +0800

    test_1.py

---
+++ test_2.py
@@ -0,0 +1,53 @@
+def save_file(boy,girl,count):
+    file_name_boy = 'boy_' + str(count) + '.txt'
+    file_name_girl = 'girl_' + str(count) + '.txt'
+
+    boy_file = open(file_name_boy,'w')
+    girl_file = open(file_name_girl,'w')
+
+    boy_file.writelines(boy)
+    girl_file.writelines(girl)
+
+    boy_file.close()
+    girl_file.close()
+
+def split_file(file_name):
+    f = open('record.txt',encoding='utf-8')
+
+    boy = []
+    girl = []
+    count = 1
+
+    for each_line in f:
+        #这里进行分割字符串操作
+        if each_line[:4] != '====':
+            (role,line_spoken) = each_line.split(':',1)
+            if role == '小甲鱼':
+                boy.append(line_spoken)
+            if role == '小客服':
+                girl.append(line_spoken)
+        
+        else:
+        #文件的分别保存操作
+            save_file(boy,girl,count)
+            
+            boy = []
+            girl = []
+            count += 1
+    file_name_boy = 'boy_' + str(count) + '.txt'
+    file_name_girl = 'girl_' + str(count) + '.txt'
+
+    boy_file = open(file_name_boy,'w')
+    girl_file = open(file_name_girl,'w')
+
+    boy_file.writelines(boy)
+    girl_file.writelines(girl)
+
+    boy_file.close()
+    girl_file.close()
+
+    f.close()
+
+split_file('record.txt')
+
+    
